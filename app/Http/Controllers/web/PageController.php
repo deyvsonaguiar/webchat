7938574b307c9dbf436e9bc64919165e3db5cc65
Deyvson <deyvsonaguiar@gmail.com>
2021-07-15 11:18:32 -0400

    Criando componente Dashboard e normalizando rota

---
+++ app/Http/Controllers/web/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PageController extends Controller
+{
+    public function welcom()
+    {
+        return view('welcome');
+    }
+
+    public function dashboard()
+    {
+        return Inertia::render('Dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Controllers\web\PageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -24,6 +14,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function(){
+    Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+});
+
